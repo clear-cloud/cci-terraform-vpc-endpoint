a40c05e2413f51f58e158f5f2674ee12a6f3f0df
Chris Morgan <chris.morgan@clearcloud.co.uk>
2019-03-20 12:53:15 +0000

    init

---
+++ main.tf
@@ -0,0 +1,9 @@
+#--------------------------------------------------------------
+# VPC Endpoint(s)
+#--------------------------------------------------------------
+
+resource "aws_vpc_endpoint" "endpoint" {
+  count             = "${length(split(",", var.service_name))}"
+  vpc_id            = "${var.vpc_id}"
+  service_name      = "${element(split(",", var.service_name), count.index)}"
+}
