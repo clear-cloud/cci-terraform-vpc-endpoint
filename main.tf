b92fa843c86c20dea83974152f82ab5f491719d2
Chris Morgan <chris.morgan@clearcloud.co.uk>
2019-03-20 13:11:33 +0000

    init

--- main.tf
+++ main.tf
@@ -3,7 +3,9 @@
 #--------------------------------------------------------------
 
 resource "aws_vpc_endpoint" "endpoint" {
-  count             = "${length(split(",", var.service_name))}"
-  vpc_id            = "${var.vpc_id}"
-  service_name      = "${element(split(",", var.service_name), count.index)}"
+  count        = "${length(split(",", var.service_name))}"
+  vpc_id       = "${var.vpc_id}"
+  service_name = "${element(split(",", var.service_name), count.index)}"
+
+  private_dns_enabled = true
 }
